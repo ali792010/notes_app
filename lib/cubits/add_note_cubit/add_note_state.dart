04531c3f80f896d5f7f96892f2ce8305a9e9c20e
ali792010 <alihos792010@gmail.com>
2024-05-29 18:57:50 +0300

    Register Adapter

---
+++ lib/cubits/add_note_cubit/add_note_state.dart
@@ -0,0 +1,8 @@
+class AddNotesState {}
+class AddNotesInitial extends AddNotesState {}
+class AddNotesLoading extends AddNotesState {}
+class AddNotesSuccess extends AddNotesState {}
+class AddNotesFailure extends AddNotesState {
+  final String errMessage;
+  AddNotesFailure(this.errMessage);
+}
